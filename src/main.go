f4350e2b63facc4d5800acdb86606667c24f7915
Trung Pham <trung@phamcom.com>
2012-09-22 17:27:02 -0700

    initial project setup

---
+++ src/main.go
@@ -0,0 +1,14 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: trungpham
+ * Date: 9/22/12
+ * Time: 5:25 PM
+ * To change this template use File | Settings | File Templates.
+ */
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("Hello world!")
+}
